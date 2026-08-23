001dba3a7a9c5fea17f9a7dec328c4d65da67707
caiocerqueira023-svg <caiocerqueira023@gmail.com>
2026-08-23 11:43:51 -0300

    teste

--- model/Personagem.java
+++ model/Personagem.java
@@ -1,4 +1,9 @@
 package model;
-// comentario de teste
-public class Personagem {
+
+public abstract class Personagem {
+    protected String nome;
+
+    protected int atrituto_1;
+    protected int atributo_2;
+    protected int atributo_3;
 }
