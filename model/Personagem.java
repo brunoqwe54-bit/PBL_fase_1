a9afcdb2697477975aa738764372c0e17867dd58
brunoqwe54-bit <brunoqwe54@gmail.com>
2026-08-23 11:28:09 -0300

    atributos genericos

--- model/Personagem.java
+++ model/Personagem.java
@@ -1,4 +1,9 @@
 package model;
 
-public class Personagem {
+public abstract class Personagem {
+    protected String nome;
+
+    protected int atrituto_1;
+    protected int atributo_2;
+    protected int atributo_3;
 }
