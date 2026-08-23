79cb1d6d9119c1cf7e712d66c2dadcfd9bb304ba
brunoqwe54-bit <brunoqwe54@gmail.com>
2026-08-23 12:05:06 -0300

    prototipo menu

---
+++ view/MenuInicial.java
@@ -0,0 +1,22 @@
+package view;
+
+import java.util.Scanner;
+
+public class MenuInicial {
+    private Scanner teclado = new Scanner(System.in);
+
+    public void exibir(){
+        System.out.println("==================");
+        System.out.println("    TITULO   ");
+        System.out.println("==================");
+        System.out.println("1 - inciar a partida");
+        System.out.println("2 - Sair do jogo);
+        int opcao = teclado.nextInt();
+
+
+
+
+    }
+}
+
+
