670392ae6e1c3c1243434d0368044e20d41072f0
brunoqwe54-bit <brunoqwe54@gmail.com>
2026-08-23 12:17:23 -0300

    main criada

---
+++ Main.java
@@ -0,0 +1,8 @@
+import view.MenuInicial;
+
+public class Main {
+    public static void main(String[] args){
+        MenuInicial MenuInicial = new MenuInicial();
+        MenuInicial.exibir();
+    }
+}
