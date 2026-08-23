4d175ed9fffa707b01ad9133b4de698ddcaaf4fa
caiocerqueira023-svg <caiocerqueira023@gmail.com>
2026-08-23 12:29:16 -0300

    Prototipo metodo para dialogo

--- model/Personagem.java
+++ model/Personagem.java
@@ -3,7 +3,14 @@ package model;
 public abstract class Personagem {
     protected String nome;
 
+    protected Personagem(String nome) {
+        this.nome = nome;
+    }
     protected int atrituto_1;
     protected int atributo_2;
     protected int atributo_3;
+
+    protected void dialogo(String mensagem) {
+        System.out.println(nome + ":" + mensagem);
+    }
 }
