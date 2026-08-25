edc97c47c4cd57965a84905733761d048b745987
brunoqwe54-bit <brunoqwe54@gmail.com>
2026-08-24 22:19:33 -0300

    feat: adiciona Menu Inicial e metodo de escolha narrativa generico na View

--- view/ViewDialogo.java
+++ view/ViewDialogo.java
@@ -2,9 +2,30 @@ package view;
 
 import model.Dialogo;
 
+import java.util.Scanner;
+
 public class ViewDialogo {
+    private Scanner teclado = new Scanner(System.in);
     public void mostrar(Dialogo dialogo) {
 
         System.out.println(dialogo.getPersonagem().getNome() + ": " + dialogo.getTexto());
+    }
+    public int exibirMenuNarrativo(Dialogo dialogo, String[] opcoesDeResposta){
+        //imprime a fala do personagem
+        System.out.println(dialogo.getPersonagem().getNome() + ": " + dialogo.getTexto());
+        System.out.println("--------------------------------");
+
+        //numero de opções de escolha
+
+        for( int i =0; i< opcoesDeResposta.length; i++ ){
+            System.out.println((i + 1) + " - " + opcoesDeResposta[i]);
+        }
+
+
+
+        //captura o que o jogador digitou e devolve para o Controller
+        System.out.print("Sua escolha: ");
+        return teclado.nextInt();
+
     }
 }
